68115c750a9bda9580ea4d8669350e3d16975bb9
snosenzo <samnosenzo@gmail.com>
2022-05-03 21:33:49 -0400

    fix typo in projects

--- src/routes/home/projects.js
+++ src/routes/home/projects.js
@@ -1,42 +1,41 @@
 export default [
   {
-    projectTitle: "PseudoCharacters",
-    skillList: ["p5.js", "javascript", "canvas-sketch"],
-    description: "Asemic Character Generation Using p5.js",
-    link: "https://github.com/snosenzo/pseudocharacters",
-    imageSrc: "/assets/pseudocharacters.png",
+    projectTitle: 'PseudoCharacters',
+    skillList: ['p5.js', 'javascript', 'canvas-sketch'],
+    description: 'Asemic Character Generation Using p5.js',
+    link: 'https://github.com/snosenzo/pseudocharacters',
+    imageSrc: '/assets/pseudocharacters.png',
   },
   {
-    projectTitle: "t-SNE Data Sculpture",
-    skillList: ["openFrameworks/C++", "machine learning"],
+    projectTitle: 't-SNE Data Sculpture',
+    skillList: ['openFrameworks/C++', 'machine learning'],
     description:
-      "3D Data Sculpture created from t-SNE algorithm processing word 2 vec dataset.",
-    link: "https://github.com/snosenzo/tsne-motionsculpture",
-    imageSrc: "/assets/tsne.png",
+      '3D Data Sculpture created from t-SNE algorithm processing word 2 vec dataset.',
+    link: 'https://github.com/snosenzo/tsne-motionsculpture',
+    imageSrc: '/assets/tsne.png',
   },
   {
-    projectTitle: "Generative Study of Breathing",
-    skillList: ["Processing/Java"],
+    projectTitle: 'Generative Study of Breathing',
+    skillList: ['Processing/Java'],
     description:
-      "Generative study of the motion and rhythm of breathing through a series of sketches.",
-    link: "https://github.com/snosenzo/BreathingSketches",
-    imageSrc: "/assets/breathing.png",
+      'Generative study of the motion and rhythm of breathing through a series of sketches.',
+    link: 'https://github.com/snosenzo/BreathingSketches',
+    imageSrc: '/assets/breathing.png',
   },
   {
-    projectTitle: "Organic Motion Typography",
-    skillList: ["Processing/Java", "Typography"],
+    projectTitle: 'Organic Motion Typography',
+    skillList: ['Processing/Java', 'Typography'],
     description:
-      "Generative agent-based system to animate text in a chaotic/organic way.",
-    link: "https://github.com/snosenzo/organic-typography",
-    imageSrc: "/assets/organic-type.png",
+      'Generative agent-based system to animate text in a chaotic/organic way.',
+    link: 'https://github.com/snosenzo/organic-typography',
+    imageSrc: '/assets/organic-type.png',
   },
   {
-    projectTitle: "7,105 Faces, In Order",
-    skillList: ["Processing/Java", "Facial Recognition", "Digital Humanities"],
+    projectTitle: '7,105 Faces, In Order',
+    skillList: ['Processing/Java', 'Facial Recognition', 'Digital Humanities'],
     description:
-      'Utilized Facial Recognition software to place 7,105 faces in order of "similarity" in order',
-    link:
-      "https://uag.pitt.edu/index.php/Gallery/81/theme_item/1/set_item_id/14225",
-    imageSrc: "/assets/faces.png",
+      'Utilized Facial Recognition software to place 7,105 faces in order of "similarity"',
+    link: 'https://uag.pitt.edu/index.php/Gallery/81/theme_item/1/set_item_id/14225',
+    imageSrc: '/assets/faces.png',
   },
 ];
